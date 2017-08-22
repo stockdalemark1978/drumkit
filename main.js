74bb7878d44ddc219a35228e18d2ff59439e370d
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-22 14:12:21 -0400

    adding in js

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ window.addEventListener('keydown', function(event){
    
 
     var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    var key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
-    console.log(audio);
-    if(!audio) return;
-
+   
+    if(!audio) return; //stops function if there is not value for that particular key
+    
+    audio.currentTime = 0; //restarts the audio file back to 0 so it will play                                  //immediately if key is pressed, even if it was already                               //playing
     audio.play();
+  
+    key.classList.add('playing');
+   
 });
+
+function removeTransition(event) {
+   if(event.propertyName !== 'transform') return; //tells the function to skip any event that occurs which is not transform
+   this.classList.remove('playing');
+}
+
+var keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
